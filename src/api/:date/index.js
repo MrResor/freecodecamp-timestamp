491bd28b5c19e0ccaaa5d0e27a26d461adb511c9
Mr.Resor <jakusienko@gmail.com>
2025-01-31 00:53:18 +0000

    move the function declaration for /api/:date endpoint

---
+++ src/api/:date/index.js
@@ -0,0 +1,16 @@
+function date(req, res) {
+    var input = Number(req.params.date);
+    console.log(req.params.date);
+    var unix, utc;
+    if (isNaN(input)) {
+        unix = Date.parse(req.params.date);
+        utc = new Date(unix).toUTCString();
+    } else {
+        unix = new Date(input).getTime();
+        utc = new Date(input).toUTCString();
+    }
+    let msg = (utc === 'Invalid Date') ? { 'error': 'Invalid Date' } : { 'unix': Number(unix), 'utc': utc };
+    res.json(msg);
+};
+
+module.exports = { date };
