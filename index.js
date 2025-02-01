1323f5be413f3b6877edaef94c621d793d5d6242
Mr.Resor <jakusienko@gmail.com>
2025-02-01 21:22:56 +0000

    remove unnecessary variable after import change in express.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var express = require('./src/express');
-var app = express.app;
+const app = require('./src/express');
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 5001, function () {
