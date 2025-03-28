c5376bf8eb7213285e22dea8e936c49b687b6dcf
Mr.Resor <jakusienko@gmail.com>
2025-03-28 13:46:28 +0000

    add logger on app startup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { app } from "./src/express.js";
+import { logger } from "./src/logger.js";
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  logger.info('Your app is listening on port ' + listener.address().port);
 });
