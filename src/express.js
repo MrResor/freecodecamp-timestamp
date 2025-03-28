19c97cad971b0c29b47b5211584767938dc206aa
Mr.Resor <jakusienko@gmail.com>
2025-03-28 13:42:26 +0000

    change console logging to winston, with discerning if request path exists

--- src/express.js
+++ src/express.js
@@ -1,12 +1,13 @@
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
 import path from 'path';
 
-import { mainView } from "./index.js";
-import { hello } from "./api/hello/index.js";
-import { empty_date } from "./api/index.js";
 import { date } from "./api/:date/index.js";
 import { docs } from "./api/docs/index.js";
+import { empty_date } from "./api/index.js";
+import { hello } from "./api/hello/index.js";
+import { logger } from "./logger.js";
+import { mainView } from "./index.js";
 
 const app = express();
 const __dirname = import.meta.dirname;
@@ -21,7 +22,24 @@ app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use((req, _, next) => {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    
+    let hasRouteToHandle = false;
+    app._router.stack.forEach((stackItem) => {
+        // check if current rout path matches route request path
+        if (stackItem.route?.path === req.path) {
+            hasRouteToHandle = true;
+        }
+    });
+
+    let msg = `${req.method} ${req.path} - ${req.ip}`
+
+    if (hasRouteToHandle) {
+        logger.info(msg);
+    } else {
+        // No matching route for this request
+       logger.error(msg);
+    }
+    
     next();
 });
 
