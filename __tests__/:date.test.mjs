a29f72bca080385e2a5030c9d52daf634ee21b9c
Mr.Resor <jakusienko@gmail.com>
2025-05-19 19:37:59 +0000

    add testcases for /api/:date endpoint

--- __tests__/:date.test.mjs
+++ __tests__/:date.test.mjs
@@ -4,10 +4,38 @@ import request from 'supertest';
 import { app } from '../src/express.mjs';
 
 describe('/api/:date', () => {
-    it("should say hello", async () => {
-        const res = await request(app).get('/api/hello');
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toEqual({ greeting: 'hello API' });
+    it("Correct unix date", async () => {
+        const res = await request(app).get('/api/0');
 
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({"unix":0,"utc":"Thu, 01 Jan 1970 00:00:00 GMT"});
+    });
+
+    it("Correct utc date", async () => {
+        const res = await request(app).get('/api/1970-01-01');
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({"unix":0,"utc":"Thu, 01 Jan 1970 00:00:00 GMT"});
+    });
+
+    it("Incorrect utc date", async () => {
+        const res = await request(app).get('/api/1970-13-13');
+
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toEqual({ 'error': 'Invalid Date' });
+    });
+
+    it("Incorrect type unix date", async () => {
+        const res = await request(app).get('/api/abc');
+
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toEqual({ 'error': 'Invalid Date' });
+    });
+
+    it("Correct negative unix date ", async () => {
+        const res = await request(app).get('/api/-1000');
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({"unix":-1000,"utc":"Wed, 31 Dec 1969 23:59:59 GMT"});
     });
 });
