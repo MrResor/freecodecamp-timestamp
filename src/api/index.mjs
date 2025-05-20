f5c55b252b4d010c59848562a40114a8ff5c4d5a
Mr.Resor <jakusienko@gmail.com>
2025-05-20 15:46:45 +0000

    add spacing

--- src/api/index.mjs
+++ src/api/index.mjs
@@ -5,6 +5,7 @@ const empty_date = express.Router();
 empty_date.get('/api', (_, res) => {
     let unix = Date.now();
     let utc = new Date(unix).toUTCString();
+    
     res.json({ 'unix': Number(unix), 'utc': utc });
 });
 
