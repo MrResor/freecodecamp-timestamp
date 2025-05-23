de64406a0f7c45ea0aa1eb0a033443cd617f98b8
Mr.Resor <jakusienko@gmail.com>
2025-05-23 20:25:48 +0000

    update linting with standard

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
-import { app } from "./src/express.mjs";
-import { logger } from "./src/logger.mjs";
+import { app } from './src/express.mjs'
+import { logger } from './src/logger.mjs'
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(3000, function () {
-  logger.info('Your app is listening on port ' + listener.address().port);
-});
+  logger.info('Your app is listening on port ' + listener.address().port)
+})
