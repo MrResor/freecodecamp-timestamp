1f2bcf0127704be31284433b75f6bb163930db92
Mr.Resor <jakusienko@gmail.com>
2025-06-09 16:59:39 +0000

    remove unneded env declaration

--- vitest.config.js
+++ vitest.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
-    env: {
-      NODE_ENV: 'production'
-    }
+    environment: 'node'
   }
 })
