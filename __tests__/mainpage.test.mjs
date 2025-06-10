72761dfc839e71fceefbfbc32395815c85030209
Mr.Resor <jakusienko@gmail.com>
2025-06-10 17:39:05 +0000

    add more tests for / endpoint, /api endpoint and check if files from public folder are served

--- __tests__/mainpage.test.mjs
+++ __tests__/mainpage.test.mjs
@@ -10,4 +10,30 @@ describe('/', () => {
     expect(res.statusCode).toBe(200)
     expect(res.text).toContain('<title>Timestamp Microservice | freeCodeCamp.org</title>')
   })
+
+  it('should return 404 for non-existing route', async () => {
+    const res = await request(app).get('/non-existing-route')
+
+    expect(res.statusCode).toBe(404)
+    expect(res.text).toContain('<title>Error</title>')
+    expect(res.headers['content-type']).toContain('text/html')
+    expect(res.text).toContain('Cannot GET /non-existing-route')
+  })
+
+  it('should return 404 for non-existing API route', async () => {
+    const res = await request(app).get('/api/non-existing-route')
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual({ error: 'Invalid Date' })
+  })
+})
+
+describe('/style.css', () => {
+  it('should return style.css', async () => {
+    const res = await request(app).get('/style.css')
+
+    expect(res.statusCode).toBe(200)
+    expect(res.headers['content-type']).toContain('text/css')
+    expect(res.text).toContain('body')
+  })
 })
