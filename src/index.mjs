bd3c6809fe256fdb34dc18248176d20291cc419d
Mr.Resor <jakusienko@gmail.com>
2025-06-15 14:29:28 +0000

    simplify serving mainpage

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,9 @@
 import express from 'express'
-import path from 'path'
 
-const __dirname = import.meta.dirname
 const mainView = express.Router()
 
 mainView.get('/', (_, res) => {
-  res.status(200).sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+  res.status(200).sendFile('./views/index.html', { root: '.' })
 })
 
 export { mainView }
